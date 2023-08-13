346aa8f52bfdaf1067482650efee475c95590af9
Usenina Maria <mariyausenina@gmail.com>
2023-08-14 00:38:50 +0400

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,107 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json, "new_data.json");
+        List<Employee> list1 = parseXML("data.xml");
+        String json2 = listToJson(list1);
+        writeString(json2, "data2.json");
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> list = null;
+
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping("id", "firstName", "lastName", "country", "age");
+
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            list = csv.parse();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static String listToJson(List<Employee> list) {
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+    public static void writeString(String json, String fileName) {
+        try (FileWriter file = new
+                FileWriter(fileName)) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Employee> parseXML(String fileName) {
+        List<Employee> employeeList = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element employee = (Element) node;
+
+                    String idString = employee.getElementsByTagName("id").item(0).getTextContent();
+                    long id = 0;
+                    if (!idString.isEmpty()) {
+                        id = Long.parseLong(idString);
+                    }
+
+                    String firstName = employee.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = employee.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = employee.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(employee.getElementsByTagName("age").item(0).getTextContent());
+                    Employee employees = new Employee(id, firstName, lastName, country, age);
+                    employeeList.add(employees);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return employeeList;
+    }
+}
+
